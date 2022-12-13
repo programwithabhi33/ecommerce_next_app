25d0e5ce1c9334bbdad032171e98f5647758f8d7
abhi <programwithabhi@gmail.com>
2022-12-13 20:38:23 +0530

    comment out the navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <>
   
-{/* <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
   <a href="https://flowbite.com/" class="flex items-center">
       <img src="/mylogo.png" class="h-6 mr-3 sm:h-9" alt="Flowbite Logo"/>
@@ -34,7 +34,7 @@ const Navbar = () => {
     </ul>
   </div>
   </div>
-</nav> */}
+</nav>
 </>
   )
 }
